802d6e38679a3642243fbcfcf5e4b36f5ae92214
fcortevargas <f.cortevargas@gmail.com>
2023-02-10 17:04:02 +0100

    Added subscriber nodes.

---
+++ src/subscriber.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+import rospy
+from geometry_msgs.msg import Pose
+
+def callback(msg: Pose):
+   rospy.loginfo(f" position=({round(msg.position.x, 2)}, {round(msg.position.y, 2)}"
+      f" orientation=({round(msg.orientation.x, 2)}, {round(msg.orientation.y, 2)},"
+      f" {round(msg.orientation.z, 2)}, {round(msg.orientation.w, 2)})")
+
+def main():
+   # Initialize the ROS system and become a node.
+   rospy.init_node("subscriber_node", anonymous=True)
+   
+   # Create a subscriber object.
+   rospy.Subscriber("qr/pose", Pose, callback)
+
+   # Let ROS take over.
+   rospy.spin()
+
+if __name__ == '__main__':
+   try:
+      main()
+   except rospy.ROSInterruptException:
+      pass
